469bd66b06f29f8159b8d5a3f52454bcf6b91776
erion-meta <metaerion79@gmail.com>
2023-02-02 22:27:39 +0100

    .

--- src/ATM_Class.java
+++ src/ATM_Class.java
@@ -7,4 +7,25 @@
 import javax.swing.*;
 
 public class ATM_Class extends JFrame {
+
+    /* Konstruktori i klases ATM
+     * Vendosim opsionet e konfigurimit, madhesine, ndertimi i programit.
+     */
+    public ATM_Class()
+    {
+        super("ATM Account!!");
+
+        for (int i=0; i <= 3; i++)
+        {
+            transaksioni[i] ="";
+        }
+        setResizable(false);
+        setLocationRelativeTo(null);
+        NdertoATM();
+        pack();
+        setSize(800,500);
+        setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
 }
